cc955c9ca632b8cfe934350d4482baf33a9de6e5
ayob metry <thetravelbird_2006@yahoo.com>
2020-10-17 11:36:22 -0400

    tryagain

---
+++ Syntax/class01/FirstProgram.java
@@ -0,0 +1,12 @@
+package Com.Syntax.class01;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("i am a programmer now !!! yay");
+
+	}
+
+}
---
+++ Syntax/class04/positaveOrNegative.java
@@ -0,0 +1,13 @@
+package Com.Syntax.class04;
+
+public class positaveOrNegative {
+	public static void main(String[] args) {
+		
+		int num= -3;
+		if(num >0 ) {
+			System.out.println("it is a positive number");
+		}else if(num<0) {
+				System.out.println("it is a negative number");
+		}
+	}
+}
